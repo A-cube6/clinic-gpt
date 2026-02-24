fa43361f9e8fb7c85739c89379e83881d3eec49b
Agnihotro Sinha <agnihotro.sinha@defence.gov.au>
2026-02-24 19:12:43 +1100

    fix error for vercel

--- components/home/doctors-from-supabase.tsx
+++ components/home/doctors-from-supabase.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { JSX, useEffect, useMemo, useState } from "react";
 import { createSupabaseBrowserClient } from "@/lib/supabase/browser";
 import { ChevronRight, MessageCircle } from "lucide-react";
 
